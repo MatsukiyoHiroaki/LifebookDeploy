4affff4d3e4a0d2367de99236d9c52563a043d8f
Hiroaki Matsukiyo <matsukiyo.h@jp.fujitsu.com>
2016-02-04 15:10:00 +0900

    firstcommit

---
+++ app/scripts/main.js
@@ -0,0 +1,42 @@
+/**
+ * This is Lifebooks module.
+ *
+ * @module Lifebooks
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('Lifebooks', [
+      'ngNewRouter',
+      'Lifebooks.config',
+      'Lifebooks.components.home',
+      'Lifebooks.components.album',
+      'Lifebooks.components.about',
+      'Lifebooks.components.camera',
+      'Lifebooks.components.contact',
+      'Lifebooks.components.calendar'
+    ])
+    .controller('AppController', AppController);
+
+  AppController.$routeConfig = [
+    {path: '/',       redirectTo: '/camera'},
+    {path: '/home',    component: 'home'},
+    {path: '/album',   component: 'album'},
+    {path: '/about',   component: 'about'},
+    {path: '/camera',  component: 'camera'},
+    {path: '/contact', component: 'contact'},
+    {path: '/calendar', component: 'calendar'}
+  ];
+
+  AppController.$inject = [];
+
+  /**
+   * AppController
+   *
+   * @class AppController
+   * @main Lifebooks
+   * @constructor
+   */
+  function AppController () {}
+})();
